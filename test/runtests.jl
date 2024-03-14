652cdd2eac3f130fbe905d79a26cad78796fbdc0
TheCedarPrince <jacobszelko@gmail.com>
2024-03-14 16:44:07 -0400

    Some initial tests for the package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
+using DataFrames
 using IPUMS
 using Test
 
-@testset "IPUMS.jl" begin
-    # Write your tests here.
+@testset "Helpers" begin
+    res = ipums_data_collections()
+    @test DataFrame == typeof(res)
+    @test (14, 4) == size(res)
 end
