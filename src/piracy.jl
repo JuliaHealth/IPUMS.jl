1244570f1a8b509ec4576d3879086ffda8b18f70
TheCedarPrince <jacobszelko@gmail.com>
2024-04-01 01:14:16 -0400

    Forgive me, a sinner

--- src/piracy.jl
+++ src/piracy.jl
@@ -16,3 +16,7 @@ end
 function OpenAPI.from_json(a::Vector{TimeSeriesTableFull}, b)
     [convert(TimeSeriesTableFull, b)]
 end
+
+function OpenAPI.from_json(a::Vector{IPUMS.Shapefile}, b)
+    [IPUMS.Shapefile(s["name"], s["year"], s["geographicLevel"], s["extent"], s["basis"], s["sequence"]) for s in b["data"]]
+end
