43f31c224237d69ab3b93c866ec32590acdcb2a6
00krishna <cyclotomiq@gmail.com>
2024-04-06 15:57:16 -0700

    added test cases for ddi parser.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,15 @@ using DataFrames
 using IPUMS
 using Test
 
+
 @testset "Helpers" begin
     res = ipums_data_collections()
     @test DataFrame == typeof(res)
     @test (14, 4) == size(res)
+end
+
+@testset "DDI Parser" begin
+    ddi = parse_ddi("testdata/cps_00157.xml")
+    @test ddi.extract_date == "2023-07-10"
+    @test ddi.variable_info[1].position_end == 4
 end
