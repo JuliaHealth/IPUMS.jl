bc349a8648da8b394a40046ad031e85b78229e1c
TheCedarPrince <jacobszelko@gmail.com>
2024-04-07 02:54:22 -0400

    Ah, but you have heard of me

--- src/piracy.jl
+++ src/piracy.jl
@@ -4,7 +4,6 @@ Behold you Julians, the sins of a blasphemer.
 
 =# 
 
-
 function OpenAPI.from_json(a::Vector{DatasetSimple}, b)
     convert.(DatasetSimple, b["data"])
 end
@@ -19,4 +18,17 @@ end
 
 function OpenAPI.from_json(a::Vector{IPUMS.Shapefile}, b)
     [IPUMS.Shapefile(s["name"], s["year"], s["geographicLevel"], s["extent"], s["basis"], s["sequence"]) for s in b["data"]]
+end
+
+function OpenAPI.from_json(a::IPUMS.DatasetFull, b::Dict{String, Any})
+    DatasetFull(
+        b["name"], 
+        b["description"],
+        b["group"],
+        b["sequence"],
+        convert.(IPUMS.DataTableSimple, b["dataTables"]),
+        convert.(IPUMS.DatasetFullGeogLevelsInner, b["geogLevels"]),
+        b["hasMultipleDataTypes"],
+        convert.(DatasetFullBreakdowns, b["breakdowns"])
+    )
 end
