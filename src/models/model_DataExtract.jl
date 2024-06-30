c3dd5f9d5762518e69970299d8ab26309e7a8fdc
MichelaRocchetti <80649722+MichelaRocchetti@users.noreply.github.com>
2024-06-30 16:45:22 +0200

    Update model_DataExtract.jl
    -add description
    -add examples
    - add return

--- src/models/model_DataExtract.jl
+++ src/models/model_DataExtract.jl
@@ -12,21 +12,31 @@ DataExtract(;
     downloadLinks=nothing,
 )
 ```
-This function  extracts data given the 
+This function extracts data given the definition of the data extracted, the link for the download and its status. 
+
 # Arguments
 
-- `extractDefinition::DataExtractDefinition`- Definition of the extracted data
-- `number::Int64`-
-- `status::String`- Status of the data extraction
-- `downloadLinks::DataExtractDownloadLinks`- Download link for the extracted data
+- `extractDefinition::DataExtractDefinition`- Definition of the extracted data.
+- `number::Int64`- Number of the data.
+- `status::String`- Status of the data extraction (eg. "complete").
+- `downloadLinks::DataExtractDownloadLinks`- Download link for the extracted data.
 
-# Return
-It returns the definition and the data with their download status and the link
+# Returns
 
-# Example
- OrderedMap { "extractDefinition": OrderedMap { "datasets": OrderedMap { "1790_cPop": OrderedMap { "dataTables": List [ "NT1" ], "geogLevels": List [ "place_00498" ] }, "1800_cPop": OrderedMap { "dataTables": List [ "NT3" ], "geogLevels": List [ "state" ] } }, "timeSeriesTables": OrderedMap { "A00": OrderedMap { "geogLevels": List [ "state" ] } }, "timeSeriesTableLayout": "time_by_row_layout", "dataFormat": "csv_no_header", "description": "abc", "version": 2, "collection": "nhgis" },
-  "number": 2, "status": "complete", "downloadLinks": OrderedMap { "codebookPreview": "https://api.ipums.org/downloads/nhgis/api/v1/extracts/1234567/nhgis0007_csv_PREVIEW.zip", "tableData": "https://api.ipums.org/downloads/nhgis/api/v1/extracts/1234567/nhgis0007_csv.zip",
-  "gisData": "https://api.ipums.org/downloads/nhgis/api/v1/extracts/1234567/nhgis0007_shape.zip" } }
+It returns the data definition,the number the status of the download and the link to download the data.
+
+# Examples
+
+```julia-repl
+
+julia> IPUMS.DataExtract(extractDefinition = "datasets",number = 2, status="complete", downloadLinks = "https://api.ipums.org/downloads/nhgis/api/v1/extracts/1234567/nhgis0007_csv_PREVIEW.zip")
+{
+  "extractDefinition": "datasets",
+  "number": 2,
+  "status": "complete",
+  "downloadLinks": "https://api.ipums.org/downloads/nhgis/api/v1/extracts/1234567/nhgis0007_csv_PREVIEW.zip"
+}
+```
 # Reference
 To find out more about the DataExtract type visit the [Reference page of IPUMS API DataExtract](https://developer.ipums.org/docs/v2/workflows/create_extracts/microdata)
 """
